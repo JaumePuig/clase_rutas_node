55b0b34299df9e3a0ef8bc9e1dbc2553ae94c922
JaumePuig <jaumepuigsoriano@gmail.com>
2026-04-21 17:38:35 +0200

    autenticacion token

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,11 @@
+import { validateToken } from "../services/token.service.js";
+
 export function authMiddleware(req, res, next){
-    console.log(req.headers.authorization);
+    
     const token = req.headers.authorization;
     if(!token) res.status(401).send("Token invalido");
+
+    const decodedToken = validateToken(token.replace('Bearer ', ''));
+    if(!decodedToken) res.status(401).send("Error de token");
     next();
 }
--- src/services/token.service.js
+++ src/services/token.service.js
@@ -3,3 +3,9 @@ export function createToken(userInfo) {
     const token = jwt.sign(userInfo, process.env.tokenKey);
     return token;
 }
+
+export function validateToken(token){
+    var decoded = jwt.verify(token, process.env.tokenKey);
+
+    return decoded;
+}
